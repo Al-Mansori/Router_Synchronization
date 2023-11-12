473e8148eb181413d6736e9eace49ed990377952
Seif <SE.SeifIbrahim@gmail.com>
2023-11-12 20:52:31 +0200

    V1

--- src/Semaphore.java
+++ src/Semaphore.java
@@ -1,18 +1,23 @@
-import java.util.LinkedList;
-import java.util.Queue;
-
 class Semaphore {
     private int permits;
 
     public Semaphore(int permits) {
-
+        this.permits = permits;
     }
 
     public synchronized void acquire() throws InterruptedException {
-
+        while (permits == 0) {
+            wait();
+        }
+        permits--;
     }
 
     public synchronized void release() {
+        permits++;
+        notify();
+    }
 
+    public synchronized boolean hasPermits() {
+        return permits > 0;
     }
 }
